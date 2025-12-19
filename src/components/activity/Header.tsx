616c32bd0ff479cbaa828b268b062a285982ec4b
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2025-12-18 19:33:38 -0600

    refactor: remove unnecessary margin from Header component for improved layout consistency

--- src/components/activity/Header.tsx
+++ src/components/activity/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   return (
     <header
       id="home"
-      className="min-h-screen h-screen flex flex-col justify-center items-center gap-4 bg-dark-blue [background-image:linear-gradient(to_right,rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-size-[80px_80px] text-center mt-12"
+      className="min-h-screen h-screen flex flex-col justify-center items-center gap-4 bg-dark-blue [background-image:linear-gradient(to_right,rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-size-[80px_80px] text-center"
     >
 
       <p className="text-sm md:text-lg uppercase tracking-[0.3em] text-white/90">
